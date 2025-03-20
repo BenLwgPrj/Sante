b079377f51d1dd1ea6aee1bb3b500816931f2c52
benoit.projet.lewagon@gmail.com <benoit.projet.lewagon@gmail.com>
2025-03-20 10:41:34 +0000

    Addition of macro for INCA format classification

--- macros/inca_format_translation.sql
+++ macros/inca_format_translation.sql
@@ -1,27 +1,67 @@
-{% macro format_statnut (val) %} -- return IMC_categorie
+{% macro classifify_format_statnut (value) %} -- return class_IMC
      CASE  
-        WHEN  {{ val }} = 0 THEN 'Maigreur'
-        WHEN  {{ val }} = 1 THEN 'Normal'
-        WHEN  {{ val }} = 2 THEN 'Risque de surpoids'
-        WHEN  {{ val }} = 3 THEN 'Surpoids'
-        WHEN  {{ val }} = 4 THEN 'Obésité'
-        WHEN  {{ val }} = 5 THEN 'Obésité morbide'
-        ELSE 'Valeur inconnue'
+        WHEN  {{ value }} = 0 THEN '1-Maigreur'
+        WHEN  {{ value }} = 1 THEN '2-Normal'
+        WHEN  {{ value }} = 2 THEN '3-Risque de surpoids'
+        WHEN  {{ value }} = 3 THEN '4-Surpoids'
+        WHEN  {{ value }} = 4 THEN '5-Obésité'
+        WHEN  {{ value }} = 5 THEN '6-Obésité morbide'
+        ELSE '0-Valeur inconnue'
     END 
 {% endmacro %} 
 
-{% macro classifify_tage_ps (tage_ps) %} -- return class_age
+{% macro classifify_tage_ps (value) %} -- return class_age
     CASE
-        WHEN val = 1 THEN "1-Enfant"
-        WHEN val = 2 THEN "1-Enfant"
-        WHEN val = 3 THEN "1-Enfant"
-        WHEN val = 4 THEN "1-Enfant"
-        WHEN val = 5 THEN "2-Ado"
-        WHEN val = 6 THEN "2-Ado"
-        WHEN val = 7 THEN "3-Adulte"
-        WHEN val = 8 THEN "3-Adulte"
-        WHEN val = 9 THEN "3-Adulte"
+        WHEN {{ value }} = 1 THEN '1-Enfant'
+        WHEN {{ value }} = 2 THEN '1-Enfant'
+        WHEN {{ value }} = 3 THEN '1-Enfant'
+        WHEN {{ value }} = 4 THEN '1-Enfant'
+        WHEN {{ value }} = 5 THEN '2-Ado'
+        WHEN {{ value }} = 6 THEN '2-Ado'
+        WHEN {{ value }} = 7 THEN '3-Adulte'
+        WHEN {{ value }} = 8 THEN '3-Adulte'
+        WHEN {{ value }} = 9 THEN '3-Adulte'
+        ELSE 
+    END '0-Valeur inconnue'
+{% endmacro %} 
+
+{% macro classifify_IMC (value) %} -- return class_IMC
+    CASE  
+--        WHEN {{ value }}  < 16 THEN '1-anorexie ou dénutrition'
+--        WHEN {{ value }} >= 16.5 AND {{ value }} < 18.5 THEN '2-état de maigreur'
+        WHEN {{ value }} < 18.5 THEN '2-état de maigreur'
+        WHEN {{ value }} >= 18.5 AND {{ value }} < 25 THEN '3-corpulence normale'
+        WHEN {{ value }} >= 25 AND {{ value }} < 30 THEN '4-surpoids'
+        WHEN {{ value }} >= 30 THEN '5-obésité'
+--        WHEN {{ value }} >= 30 AND {{ value }} < 35 THEN '5-obésité modérée de classe 1'
+--        WHEN {{ value }} >= 35 AND {{ value }} <= 40 THEN '6-obésité sévère de classe 2'
+--        WHEN {{ value }} > 40 THEN '7-obésité morbide ou massive'
+        ELSE '0-Valeur inconnue'
     END 
 {% endmacro %} 
 
+{% macro classifify_fumeur (value) %} -- return class_fumeur
+    CASE
+        WHEN {{ value }} = 1 THEN 'Fumeur'
+        WHEN {{ value }} = 2 THEN 'Fumeur'
+--        WHEN {{ value }} = 3 THEN 'Non_fumeur'
+--        WHEN {{ value }} = 4 THEN 'Non_fumeur'
+        ELSE 'Non_fumeur'
+    END
+{% endmacro %} 
 
+{% macro classifify_revenu (value) %} -- return class_revenue
+    CASE
+        WHEN {{ value }} >= 1 AND {{ value }} <= 7 THEN '1-Revenus_bas'
+        WHEN {{ value }} > 7 AND {{ value }}  <= 11 THEN '2-Revenus_moyens'
+        WHEN {{ value }} > 11 THEN '3-Revenus_eleves'
+        ELSE '0-Valeur inconnue'
+    END 
+{% endmacro %} 
+
+{% macro classifify_sex_ps (value) %} -- return class_sexe
+    CASE 
+        WHEN {{ value }} = 1 THEN 'Homme'
+        ELSE 'Femme'
+    END
+{% endmacro %} 
--- models/mart/INCA_indiv_join.sql
+++ models/mart/INCA_indiv_join.sql
@@ -1,53 +1,18 @@
 SELECT  
-  *,
-    CASE
-        WHEN tage_ps = 1 THEN "1-Enfant"
-        WHEN tage_ps = 2 THEN "1-Enfant"
-        WHEN tage_ps = 3 THEN "1-Enfant"
-        WHEN tage_ps = 4 THEN "1-Enfant"
-        WHEN tage_ps = 5 THEN "2-Ado"
-        WHEN tage_ps = 6 THEN "2-Ado"
-        WHEN tage_ps = 7 THEN "3-Adulte"
-        WHEN tage_ps = 8 THEN "3-Adulte"
-        WHEN tage_ps = 9 THEN "3-Adulte"
-    END AS class_age,
-    CASE
-        WHEN fume = 1 THEN "Fumeur"
-        WHEN fume = 2 THEN "Fumeur"
-        WHEN fume = 3 THEN "Non_fumeur"
-        WHEN fume = 4 THEN "Non_fumeur"
-    ELSE "Non_fumeur"
-    END AS class_fumeur,
-    CASE  
---        WHEN IMC < 16 THEN '1-anorexie ou dénutrition'
---        WHEN IMC >= 16.5 AND IMC < 18.5 THEN '2-état de maigreur'
-        WHEN IMC < 18.5 THEN '2-état de maigreur'
-        WHEN IMC >= 18.5 AND IMC < 25 THEN '3-corpulence normale'
-        WHEN IMC >= 25 AND IMC < 30 THEN '4-surpoids'
-        WHEN IMC >= 30 THEN '5-obésité'
---        WHEN IMC >= 30 AND IMC < 35 THEN '5-obésité modérée de classe 1'
---        WHEN IMC >= 35 AND IMC <= 40 THEN '6-obésité sévère de classe 2'
---        WHEN IMC > 40 THEN '7-obésité morbide ou massive'
-        ELSE 'Valeur inconnue'
-    END AS class_IMC,
-    CASE
-        WHEN revenu>= 1 AND revenu<= 7 THEN 'Revenus_bas'
-        WHEN revenu> 7 AND revenu <= 11 THEN 'Revenus_moyens'
-        WHEN revenu> 11 THEN 'Revenus_eleves'
-        ELSE 'Unknown'
-    END AS class_revenue,
-    CASE 
-        WHEN sex_ps=1 THEN 'Homme'
-        ELSE 'Femme'
-    END AS class_sexe,
+    *,
+    {{ classifify_tage_ps('tage_ps') }} AS class_age,
+    {{ classifify_fumeur('fume') }} AS class_fumeur,
+    {{ classifify_IMC('IMC') }} AS class_IMC,
+    {{ classifify_revenu('revenu') }} AS class_revenue,
+    {{ classifify_sex_ps('sex_ps') }} AS class_sexe,
 
-FROM {{ ref('int_indiv-descrip') }} AS indiv
-INNER JOIN {{ ref('int_actphys-sedent') }} AS actphys
+FROM {{ ref('int_indiv-descrip') }} 
+INNER JOIN {{ ref('int_actphys-sedent') }} 
   USING (NOIND)
-INNER JOIN {{ ref('int_indiv_habit') }} AS habits_indiv
+INNER JOIN {{ ref('int_indiv_habit') }} 
   USING(NOIND)
-INNER JOIN {{ ref('int_men_habitudes') }} AS men_habitudes
+INNER JOIN {{ ref('int_men_habitudes') }} 
   USING (NOMEN)
-INNER JOIN {{ ref('int_conso_gpe_inca3') }} AS conso
+INNER JOIN {{ ref('int_conso_gpe_inca3') }} 
   USING (NOIND)
 
